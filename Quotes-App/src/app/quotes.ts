5cdc33d76517ac1a4ca05b5524232c284c1d01a2
Joe Gakumo <joegakumo1@gmail.com>
2021-08-20 17:41:40 +0300

    add output propert to quotes.ts

--- Quotes-App/src/app/quotes.ts
+++ Quotes-App/src/app/quotes.ts
@@ -1,7 +1,11 @@
 export class Quotes {
-    name:string;
-    tittleQuote:string;
-    messageQuote:string;
-    author:string;
+    showQuoteMessageAndAuthor:boolean;
+    constructor(public name:string, public tittleQuote:string, public messageQuote:string, public author:string){
+        this.showQuoteMessageAndAuthor=false;
+    }
+    // name:string;
+    // tittleQuote:string;
+    // messageQuote:string;
+    // author:string;
 
 }
