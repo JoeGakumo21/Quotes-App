4edce4c97aa6f3bb1c0e0c14f3055b8e9e5cbcc0
Joe Gakumo <joegakumo1@gmail.com>
2021-08-20 17:45:27 +0300

    add constuctor in quotes.component.ts

--- Quotes-App/src/app/quotes/quotes.component.ts
+++ Quotes-App/src/app/quotes/quotes.component.ts
@@ -1,3 +1,4 @@
+import { Quote } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { Quotes } from '../quotes';
 @Component({
@@ -8,7 +9,7 @@ import { Quotes } from '../quotes';
 export class QuotesComponent implements OnInit {
 
   myQuote:Quotes[]=[
-    {name:"Joseph Gakumo ",tittleQuote:"Motivation Quote",messageQuote:"Success is not Final, Failure is not Fatal, Its the courage to continue that counts",author:"JoeKumsy Kenyan"},
+    new Quotes("Joseph Gakumo ","Motivation Quote","Success is not Final, Failure is not Fatal, Its the courage to continue that counts","JoeKumsy Kenyan"),
     // {name:"Teresia Wairimu ",tittleQuote:"Super Motivation Quote",messageQuote:"It is only when we take chances, when our lives improve. The initial and the most difficult risk that we need to take is to become honest.",author:"Walter Anderson"},
     // {name:"Moses Mugo ",tittleQuote:"Motivation Quote",messageQuote:"Nature has given us all the pieces required to achieve exceptional wellness and health, but has left it to us to put these pieces together.",author:"JDiane McLaren"},
     // {name:"Nancy Munyui",tittleQuote:"Motivation Quote",messageQuote:"When you change your thoughts, remember to also change your world.",author:"Norman Vincent Peale"},
