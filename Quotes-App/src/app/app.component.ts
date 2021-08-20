678b54011d1061f9cce115a98826ec083f670565
Joe Gakumo <joegakumo1@gmail.com>
2021-08-20 16:18:44 +0300

    add quotes component

--- Quotes-App/src/app/app.component.ts
+++ Quotes-App/src/app/app.component.ts
@@ -6,12 +6,5 @@ import { Quotes } from './quotes';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  myQuote:Quotes[]=[
-    {name:"Joseph Gakumo ",tittleQuote:"Motivation Quote",messageQuote:"Success is not Final, Failure is not Fatal, Its the courage to continue that counts",author:"JoeKumsy Kenyan"},
-    {name:"Teresia Wairimu ",tittleQuote:"Super Motivation Quote",messageQuote:"It is only when we take chances, when our lives improve. The initial and the most difficult risk that we need to take is to become honest.",author:"Walter Anderson"},
-    {name:"Moses Mugo ",tittleQuote:"Motivation Quote",messageQuote:"Nature has given us all the pieces required to achieve exceptional wellness and health, but has left it to us to put these pieces together.",author:"JDiane McLaren"},
-    {name:"Nancy Munyui",tittleQuote:"Motivation Quote",messageQuote:"When you change your thoughts, remember to also change your world.",author:"Norman Vincent Peale"},
-  ];
-
-
-}
+  
+ }
