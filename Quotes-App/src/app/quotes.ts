3246e88c5b87571d348349c7abfcfed2d87fbb5b
Joe Gakumo <joegakumo1@gmail.com>
2021-08-21 19:23:44 +0300

    minor charges

--- Quotes-App/src/app/quotes.ts
+++ Quotes-App/src/app/quotes.ts
@@ -1,6 +1,6 @@
 export class Quotes {
     showQuoteMessageAndAuthor:boolean;
-    constructor(public name:string, public tittleQuote:string, public messageQuote:string, public author:string){
+    constructor(public name:string, public tittleQuote:string, public messageQuote:string, public author:string, public postedOn:any){
         this.showQuoteMessageAndAuthor=false;
     }
     // name:string;
