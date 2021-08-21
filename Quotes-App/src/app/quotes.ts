f8555f5ba953f2f9dab1a081e650d1a207b42f9b
Joe Gakumo <joegakumo1@gmail.com>
2021-08-21 20:58:17 +0300

    add date

--- Quotes-App/src/app/quotes.ts
+++ Quotes-App/src/app/quotes.ts
@@ -1,6 +1,6 @@
 export class Quotes {
     showQuoteMessageAndAuthor:boolean;
-    constructor(public name:string, public tittleQuote:string, public messageQuote:string, public author:string, public postedOn:any){
+    constructor(public name:string, public tittleQuote:string, public messageQuote:string, public author:string, public postedOn:Date){
         this.showQuoteMessageAndAuthor=false;
     }
     // name:string;
